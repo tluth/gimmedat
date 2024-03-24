0fe3bb6d7fa4e35390cb172e7ae03883cd75b1a5
Charles Harris <char5653@uni.sydney.edu.au>
2024-03-24 21:37:48 +0100

    add header component

--- frontend-vite/src/App.tsx
+++ frontend-vite/src/App.tsx
@@ -1,5 +1,11 @@
+import Header from "./components/Header";
+
 function App() {
-  return <div className="bg-red-200"></div>;
+  return (
+    <div className="text-center bg-black h-screen flex flex-col">
+      <Header />
+    </div>
+  );
 }
 
 export default App;
