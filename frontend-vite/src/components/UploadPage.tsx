93d5794532591043f2a870d2097e50b86eaf08fe
Charles Harris <char5653@uni.sydney.edu.au>
2024-03-24 22:10:21 +0100

    add UploadPage component

---
+++ frontend-vite/src/components/UploadPage.tsx
@@ -0,0 +1,119 @@
+import React, { useState } from "react";
+
+import CustomDropzone from "./CustomDropzone.js";
+import Expire from "./Expire.js";
+import { API } from "../constants.js";
+import FileLink from "./FileLink.js";
+
+const UploadPage = () => {
+  const [file, setFile] = useState<File>();
+  const [progressVal, setProgressVal] = useState(0);
+  const [fileId, setFileId] = useState(null);
+
+  //event handlers
+  const handleFileChange = (files: File[]) => {
+    const reader = new FileReader();
+    if (files && files.length > 0) {
+      const file = files[0];
+      reader.onloadend = () => {
+        setFile(file);
+      };
+      reader.readAsDataURL(file);
+    }
+  };
+
+  const handleSubmit = () => {
+    const upload_request = {
+      file_name: file?.name,
+      byte_size: file?.size,
+      file_type: file?.type,
+    };
+    const xhr = new XMLHttpRequest();
+    xhr.open("POST", `${API}/file`);
+    xhr.setRequestHeader("Content-Type", "application/json");
+
+    xhr.onreadystatechange = function () {
+      if (xhr.readyState === 4) {
+        if (xhr.status === 200) {
+          const response = JSON.parse(xhr.responseText);
+
+          const xhrUpload = new XMLHttpRequest();
+          // create event listener to update put progress so user wont refresh page during upload
+          xhrUpload.open("PUT", response.presigned_upload_url);
+          xhrUpload.upload.addEventListener(
+            "progress",
+            function (evt) {
+              if (evt.lengthComputable) {
+                setProgressVal((evt.loaded / evt.total) * 100);
+              }
+            },
+            false
+          );
+          xhrUpload.setRequestHeader("Content-Type", file!.type);
+          xhrUpload.setRequestHeader("x-amz-acl", "private");
+          xhrUpload.onreadystatechange = function () {
+            if (xhrUpload.readyState === 4) {
+              if (xhrUpload.status === 200) {
+                setFileId(response.uuid);
+              }
+            }
+          };
+          xhrUpload.send(file);
+        } else {
+          alert("Problem submitting file.");
+        }
+      }
+    };
+    xhr.send(JSON.stringify(upload_request));
+  };
+
+  return (
+    <div className="mx-auto max-w-[70%] min-w-[50%] pt-[5%] inline-block">
+      <div>
+        <CustomDropzone onDrop={handleFileChange} dropzoneText={``} />
+      </div>
+      <button onClick={handleSubmit}>Submit</button>
+      {progressVal ? (
+        <div className="pb-6 max-w-[70%] min-w-[50%] text-custom-green">
+          <Expire
+            trigger={"programmatical"}
+            delay={0}
+            isVisible={progressVal === 100 ? false : true}
+          >
+            <div className="progress">
+              <div
+                className="progress-bar progress-bar-success progres
+                s-bar-striped"
+                role="progressbar"
+                aria-valuenow={progressVal}
+                aria-valuemin={0}
+                aria-valuemax={100}
+                style={{
+                  width: progressVal + "%",
+                }}
+              ></div>
+            </div>
+          </Expire>
+        </div>
+      ) : null}
+      {progressVal === 100 && (
+        <div className="pb-6 max-w-[70%] min-w-[50%] text-custom-green">
+          <Expire trigger={"time"} delay={3000} isVisible={false}>
+            <div
+              className="w-[20px] text-[1.4em] pl-[1%] text-center inline-block float-left text-custom-green"
+              role="img"
+              aria-label="ok"
+            >
+              👌 Upload complete
+            </div>
+          </Expire>
+        </div>
+      )}
+      {fileId && (
+        <FileLink sharingLink={`${window.location.origin}/sharing/${fileId}`} />
+      )}
+    </div>
+  );
+};
+
+export default UploadPage;
--- frontend-vite/tailwind.config.js
+++ frontend-vite/tailwind.config.js
@@ -2,7 +2,26 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontSize: {
+        custom: "calc(1.1vw + 1.1vh)", // Custom font size
+      },
+      colors: {
+        "custom-green": "rgb(73, 247, 4)", // Custom green color
+      },
+      fontFamily: {
+        mono: [
+          "ui-monospace",
+          "SFMono-Regular",
+          "Menlo",
+          "Monaco",
+          "Consolas",
+          "Liberation Mono",
+          "Courier New",
+          "monospace",
+        ], // Ensure monospace is defined
+      },
+    },
   },
   plugins: [],
 };
