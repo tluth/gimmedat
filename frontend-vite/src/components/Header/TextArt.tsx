acad0fe8a70be53a099a9d469e5c354bcc7c7461
Charles Harris <char5653@uni.sydney.edu.au>
2024-04-01 20:59:33 +0200

    make header thicker

--- frontend-vite/src/components/Header/TextArt.tsx
+++ frontend-vite/src/components/Header/TextArt.tsx
@@ -7,6 +7,7 @@ function TextArt({ label, text }: TextArtProps) {
   return (
     <pre
       aria-label={label}
+      style={{ textShadow: "1px 0 currentColor" }}
       className="bg-dark text-main font-mono text-[calc(0.25vw+0.25vh)] leading-none p-4 whitespace-pre overflow-x-hidden"
     >
       {text}
