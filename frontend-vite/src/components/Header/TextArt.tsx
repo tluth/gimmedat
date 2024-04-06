1d28c9d5ebc5c247848e942a05d786b35f8bc8dd
tluth <tom.lutherborrow@gmail.com>
2024-04-06 17:47:05 +0200

    adjust header text shadow and add fineprint re: filesize limit

--- frontend-vite/src/components/Header/TextArt.tsx
+++ frontend-vite/src/components/Header/TextArt.tsx
@@ -7,7 +7,7 @@ function TextArt({ label, text }: TextArtProps) {
   return (
     <pre
       aria-label={label}
-      style={{ textShadow: "1px 0 currentColor" }}
+      style={{ textShadow: "1px -1px 1px rgba(77, 140, 255, 0.9)" }}
       className="bg-night text-main font-mono text-[calc(0.25vw+0.25vh)] leading-none p-4 whitespace-pre overflow-x-hidden"
     >
       {text}
