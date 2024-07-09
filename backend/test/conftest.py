92174bd3c04d575999d47e270fea85cb04200101
tluth <tom.lutherborrow@gmail.com>
2024-07-09 22:10:38 +0200

    fix lint

--- backend/test/conftest.py
+++ backend/test/conftest.py
@@ -1,11 +1,11 @@
 import pytest
 
 from fastapi.testclient import TestClient
-import boto3
-from botocore.stub import Stubber
+# import boto3
+# from botocore.stub import Stubber
 
 from ..api.app import create_api
-from ..api.utils import s3_con
+# from ..api.utils import s3_con
 
 
 @pytest.fixture()
@@ -14,11 +14,10 @@ def http_client() -> TestClient:
     return TestClient(app)
 
 
-
 # https://adamj.eu/tech/2019/04/22/testing-boto3-with-pytest-fixtures/
-@pytest.fixture(autouse=True)
-def s3_stub():
-    stubber = Stubber(s3_con)
-    with Stubber(s3.meta.client) as stubber:
-        yield stubber
-        stubber.assert_no_pending_responses()
+# @pytest.fixture(autouse=True)
+# def s3_stub():
+#     stubber = Stubber(s3_con)
+#     with Stubber(s3.meta.client) as stubber:
+#         yield stubber
+#         stubber.assert_no_pending_responses()
