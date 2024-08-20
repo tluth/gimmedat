240a4fac7218ec328929bdeb85324d51638ee580
tluth <tom.lutherborrow@gmail.com>
2024-08-20 16:22:16 +0200

    add ses resources

--- infra/ses.tf
+++ infra/ses.tf
@@ -1,8 +1,8 @@
-# module "ses" {
-#   source = "./vendor/modules/cloudposse/terraform-aws-ses"
-#   domain        = local.product_domain
-#   zone_id       = data.aws_route53_zone.zone.id
-#   verify_dkim   = true
-#   verify_domain = true
-#   context = module.this.context
-# }
+module "ses" {
+  source = "./vendor/modules/cloudposse/terraform-aws-ses"
+  domain        = local.product_domain
+  zone_id       = data.aws_route53_zone.zone.id
+  verify_dkim   = true
+  verify_domain = true
+  context = module.this.context
+}
